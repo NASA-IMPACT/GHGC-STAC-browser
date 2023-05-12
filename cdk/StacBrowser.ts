07e3d944057e182110e0450f8a22b6b56ce7b01e
Slesa Adhikari <slesaad@gmail.com>
2023-05-12 13:57:01 -0500

    Update stack, hide secrets, add deploy env options

--- cdk/StacBrowser.ts
+++ cdk/StacBrowser.ts
@@ -1,4 +1,4 @@
-import { Stack, aws_s3 as s3, aws_s3_deployment as s3_deployment, aws_cloudfront as cloudfront, aws_cloudfront_origins as cloudfront_origins, StackProps} from "aws-cdk-lib";
+import { Stack, aws_s3 as s3, aws_s3_deployment as s3_deployment, StackProps} from "aws-cdk-lib";
 import { RemovalPolicy, CfnOutput } from "aws-cdk-lib";
 import { PolicyStatement, ServicePrincipal, Effect } from "aws-cdk-lib/aws-iam";
 
@@ -9,23 +9,24 @@ export class StacBrowser extends Stack {
     constructor(scope: Construct, id: string, props: Props) {
         super(scope, id);
 
-        const bucket = new s3.Bucket(this, 'Bucket', {
-            accessControl: s3.BucketAccessControl.PRIVATE,
+        const bucket = new s3.Bucket(this, `${Stack.of(this).stackName}-static-bucket`, {
+            publicReadAccess: true,
             removalPolicy: RemovalPolicy.DESTROY,
-            })
+            websiteIndexDocument: "index.html"
+        })
 
         bucket.addToResourcePolicy(new PolicyStatement({
-                    sid: 'AllowCloudFrontServicePrincipal',
-                    effect: Effect.ALLOW, 
-                    actions: ['s3:GetObject'],
-                    principals: [new ServicePrincipal('cloudfront.amazonaws.com')],
-                    resources: [bucket.arnForObjects('*')],
-                    conditions: {
-                        'StringEquals': {
-                            'aws:SourceArn': props.cloudFrontDistributionArn
-                        }
-                    }
-                }));
+            sid: 'AllowCloudFrontServicePrincipal',
+            effect: Effect.ALLOW, 
+            actions: ['s3:GetObject'],
+            principals: [new ServicePrincipal('cloudfront.amazonaws.com')],
+            resources: [bucket.arnForObjects('*')],
+            conditions: {
+                'StringEquals': {
+                    'aws:SourceArn': props.cloudFrontDistributionArn
+                }
+            }
+        }));
         
 
         new s3_deployment.BucketDeployment(this, 'BucketDeployment', {
@@ -42,12 +43,8 @@ export class StacBrowser extends Stack {
 }
 
 export interface Props extends StackProps {
-
     // ARN of the cloudfront distribution to which we should grant read access to the browser bucket. 
     cloudFrontDistributionArn: string;
-
     // location of the stac-browser dist directory in the local filesystem
     stacBrowserDistPath: string;
-
-
 }
